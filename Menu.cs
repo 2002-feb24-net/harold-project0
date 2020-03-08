a0cbe3aa0a20cd0f98a657cae1ddf7e2e790abed
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-03-07 21:06:37 -0600

    Started creating menu class [not sure if it will be instance or static yet]

---
+++ Menu.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace harold_project0
+{
+    class Menu
+    {
+        static void DisplayMenu()
+        {
+            System.Console.WriteLine("Welcome to Backgammon pizza");
+            System.Console.WriteLine("Type an option:");
+            System.Console.WriteLine("1. New Order");
+            System.Console.WriteLine("2. Check Order");
+                // current pizza order, sign in
+            System.Console.WriteLine("3. See Past Orders");
+            System.Console.WriteLine("4. Nearest Location");
+                // return one option or a list
+                // Hardcoded for arlington?
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace harold_project0
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //menu static call or object
         }
     }
 }
