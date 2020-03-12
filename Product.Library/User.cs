b419b1ea8b09a7f91e9a5f00dd25e0d0bb6821f3
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-03-11 21:34:40 -0500

    Made user class to hold username and password. Will have to make it hold order history? (or should order hold user)?

---
+++ Product.Library/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.Library
+{
+    public class User
+    {
+        private string Username
+        {
+            get { return Username; }
+            set { Username = InputUserValidation(value); }
+        }
+        private string Password
+        {
+            get { return Password; }
+            set { }
+        }
+
+        public User(string username, string password )
+        {
+
+        }
+
+        private string InputUserValidation(string username)
+        {
+            if (username == ""
+                || username == null)
+                return "Error: username field is empty.";
+            else
+            {
+                return username;
+            }
+
+        }
+
+        private string InputPasswordValidation(string password)
+        {
+            char[] specialCharacters = {'!', '@', '#',
+                                        '$', '%', '^',
+                                        '&', '*', '_',
+                                         '-', '+', '='};
+            if (password == ""
+                || password == null)
+                return "Error: password field is empty.";
+            else if (password.Length < 9)
+                return "Error: password must be longer than 8 characters";
+            else if (password.IndexOfAny(specialCharacters) == -1)
+            { // method return -1 if none of those characters are in the string
+                return "Error: password must contain at least one special character" +
+                    " (ex: ! @ # $ % ^ & *)";
+            }
+            else
+            {
+                return password;
+            }
+        }
+    }
+}
