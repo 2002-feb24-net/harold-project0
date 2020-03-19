dfecec53b2a23d9150fa8a0b1b22442ae011f38e
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-03-19 09:19:07 -0500

    started making a class that implements DbContext

---
+++ Product.Library/RestaurantContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.Library
+{
+    class RestaurantContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Store> Stores { get; set; }
+
+        // Need to add other classes that will be stored and loaded from database
+
+    }
+}
