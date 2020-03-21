837ae04f9108b2cea5b6856b484cfe46567dae95
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-03-20 21:08:19 -0500

    linked project to empty database

---
+++ Restaurant.Library.SavingAndLoading/Models/DbRestaurantContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Restaurant.Library.SavingAndLoading.Models
+{
+    public partial class DbRestaurantContext : DbContext
+    {
+        public DbRestaurantContext()
+        {
+        }
+
+        public DbRestaurantContext(DbContextOptions<DbRestaurantContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:2002-training-altamirano.database.windows.net,1433;Initial Catalog=DbRestaurant;Persist Security Info=False;User ID=haroldo;Password=EllieIs#1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
