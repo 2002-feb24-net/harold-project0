324f06708a5c39d63195791c2db00feb4bf68f2c
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-03-24 03:43:00 -0500

    added inventory data access class

--- Restaurant.Library/Inventory.cs
+++ Restaurant.Library/Inventory.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Restaurant.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Restaurant.Library
 {
-    class Inventory
+    public class Inventory : IDataInventory
     {
         // link each product instance with one store
+        public int InventoryId { get; }
+
+        public int? Quantity { get; set; }
+        public int? StoreId { get; set; }
+        public int ProductId { get; set; }
     }
 }
