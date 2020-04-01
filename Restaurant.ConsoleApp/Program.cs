5cba192e415d4e9a79a659ce1db5a26f235a2ab2
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-04-01 10:08:19 -0500

    Fixed issue #2 by making the switch statement condition in Program.cs into a string rather than an int that has to be converted to. Less bugs from converting/less exceptions

--- Restaurant.ConsoleApp/Program.cs
+++ Restaurant.ConsoleApp/Program.cs
@@ -107,17 +107,17 @@ namespace Restaurant.ConsoleApp
             while (quitFlag == false)
                 {
                 Menu.CustMenu(s_Stores, customer);
-                var loggedInOptions = Convert.ToInt32(Console.ReadLine());
+                var loggedInOptions = Console.ReadLine();
 
-                switch (loggedInOptions)
+                switch (loggedInOptions) // strings of numbers (to avoid exceptions when converting to switch statement condition")
                 {
-                    case 1:
+                    case "1":
                         //login
                         Console.WriteLine("Place an order");
                         AddOrder(customer, store);
 
                         break;
-                    case 2:
+                    case "2":
                         //register
                         Console.WriteLine("Showing your order history");
                         ShowOrderHistory(customer);
